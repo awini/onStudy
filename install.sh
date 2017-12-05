baf89963a72c2d5598169a82bbd4abb7fa306a00
wind <aa.veter@gmail.com>
2017-12-05 19:34:25 +0300

    bower and bootstrap

---
+++ install.sh
@@ -0,0 +1,8 @@
+npm i -g bower
+bower init
+bower i -S bootstrap
+bower i -S jquery
+bower i -S html5shiv
+bower i -S respond
+respond
+bower i -D mocha
--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 # coding: utf-8
+from os.path import join, exists
+
 import tornado.ioloop
 import tornado.web
 
+
+BOWER_COMPONENTS = 'bower_components'
+
+
 class MainHandler(tornado.web.RequestHandler):
+
     def get(self):
-        self.write("Hello, world")
+        return self.render("template/main.html")
+
+
+class AssetsLibHandler(tornado.web.RequestHandler):
+
+    def get(self, type, lib, filename):
+        dist_variants = ()
+        if type == "js":
+            dist_variants = ("dist", "dist/js", "dest")
+            self.set_header("Content-Type", "text/js")
+        elif type == "css":
+            dist_variants = ("dist/css",)
+            self.set_header("Content-Type", "text/css")
+
+        for dist in dist_variants:
+            fullpath = join(BOWER_COMPONENTS, lib, dist, filename)
+            if exists(fullpath):
+                self.write(open(fullpath).read())
+                return
+        self.send_error(404)
+
+class CssHandler(tornado.web.RequestHandler):
+
+    def get(self, filename):
+        self.set_header("Content-Type", "text/css")
+
+        fullpath = join("template", "css", filename)
+        if exists(fullpath):
+            self.write(open(fullpath).read())
+            return
+        self.send_error(404)
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/([^/]*)/([^/]*)/([^/]*)", AssetsLibHandler),
+        (r"/css/([^/]*)", CssHandler),
     ])
+
 
 if __name__ == "__main__":
     app = make_app()
