343f7534e5a5a70965e16195bf7bf6ec72b603c8
Андрей А. Андреев <aa.veter@gmail.com>
2017-12-12 17:35:00 +0300

    room for see stream

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -1,5 +1,6 @@
 from handlers.BaseHandler import BaseHandler
 import tornado.web
+from tornado import gen, web, httpclient
 
 
 class MainHandler(BaseHandler):
@@ -17,3 +18,31 @@ class RoomHandler(tornado.web.RequestHandler):
 hello!
 </body>
 </html>''')
+
+
+class StreamingHandler(web.RequestHandler):
+
+    @web.asynchronous
+    @gen.coroutine
+    def get(self):
+        client = httpclient.AsyncHTTPClient()
+
+        #self.write('some opening')
+        #self.flush()
+
+        requests = [
+            httpclient.HTTPRequest(
+                url='http://localhost:8889',
+                streaming_callback=self.on_chunk
+            )
+        ]
+
+        # `map()` doesn't return a list in Python 3
+        yield list(map(client.fetch, requests))
+
+        #self.write('some closing')
+        self.finish()
+
+    def on_chunk(self, chunk):
+        self.write('some chunk')
+        self.flush()
