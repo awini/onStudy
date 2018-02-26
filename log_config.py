68cdbf3a7a2befe7b81e5ce5cfdf424dd1034d8c
Aleksandr Rebrik <ackud@mail.ru>
2018-02-26 23:19:02 +0300

    query logs in different file

--- log_config.py
+++ log_config.py
@@ -30,6 +30,17 @@ config = {
             'delay': 0,
             'filename': 'logs/log.log'
         },
+        'query_to_file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'default_fmt',
+            'mode': 'a',
+            'maxBytes': 15*1024*1024,
+            'backupCount': 3,
+            'encoding': None,
+            'delay': 0,
+            'filename': 'logs/query.log'
+        },
     },
     'loggers': {
         'handlers': {
@@ -53,8 +64,8 @@ config = {
             'propagate': False,
         },
         'sqlalchemy.engine': {
-            'handlers': handlers,
-            'level': 0,
+            'handlers': ['query_to_file'],
+            'level': logging.INFO,
             'propagate': False,
         }
     },
@@ -73,5 +84,8 @@ def load_config(debug=False):
 
 def set_level(lvl):
     for loggers in config['loggers']:
+        if 'sqlalchemy.engine' == loggers:
+            # skip change level for logger sqlalchemy.engine
+            continue
         config['loggers'][loggers]['level'] = lvl
     #logging.getLogger('sqlalchemy.engine').setLevel(lvl)
